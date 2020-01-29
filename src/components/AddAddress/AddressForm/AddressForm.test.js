ccc5d3c9402823c5def59355fdedc2a30922c8b2
theseanco <theseancotterill@live.com>
2020-01-29 14:14:12 +0000

    added form input tests

--- src/components/AddAddress/AddressForm/AddressForm.test.js
+++ src/components/AddAddress/AddressForm/AddressForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, getByLabelText } from '@testing-library/react';
+import { render, getByLabelText, fireEvent } from '@testing-library/react';
 import AddressForm from './AddressForm';
 
 describe('AddressForm tests', () => {
@@ -9,4 +9,24 @@ describe('AddressForm tests', () => {
     expect(getByLabelText(container, /Notes/)).toBeInTheDocument();
     expect(getByLabelText(container, /Location/)).toBeInTheDocument();
   })
+
+
+  describe('Test form inputs', () => {
+    // const { container } = render(<AddressForm />)
+    // Simulate form inputs
+    it('Name should take an input', () => {
+    const { container } = render(<AddressForm />)
+      const input = getByLabelText(container, /Name/);
+      fireEvent.change(input, { target: { value: 'Sean' } } );
+      expect(input.value).toBe('Sean');
+    })
+
+    it('Notes should take an input', () => {
+    const { container } = render(<AddressForm />)
+      const input = getByLabelText(container, /Notes/);
+      fireEvent.change(input, { target: { value: 'Some notes about Sean' } } );
+      expect(input.value).toBe('Some notes about Sean');
+    })
+  })
+
 });
