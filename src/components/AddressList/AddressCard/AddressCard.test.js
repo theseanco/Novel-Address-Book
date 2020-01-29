2662dfc366b72bde91851c8f4cc3ef102cb347e7
theseanco <theseancotterill@live.com>
2020-01-29 20:00:06 +0000

    removed unused dependency from AddressCard tests

--- src/components/AddressList/AddressCard/AddressCard.test.js
+++ src/components/AddressList/AddressCard/AddressCard.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, getByText, waitForNextUpdate, wait } from '@testing-library/react';
+import { render, getByText, wait } from '@testing-library/react';
 import AddressCard from './AddressCard';
 import * as reverseGeocoding from '../../../utilities/reverseGeocode';
 import { act } from 'react-dom/test-utils';
