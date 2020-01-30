796544c1a8c3261192239a161a3e6013754ac188
theseanco <theseancotterill@live.com>
2020-01-30 19:26:30 +0000

    added function to post geodata

---
+++ src/utilities/addAddressToDB.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const postToDB = async (name, notes, location) => {
+  // Match data schema to google data schame
+  const processedLocation = {
+    latitude: location.lat,
+    longitude: location.lng
+  };
+  try {
+   const post = await axios.post('https://novel-address-book-backend.herokuapp.com/addresses', {
+      name,
+      notes,
+      location: processedLocation
+    })
+    return post;
+  } catch (error) {
+    return error;
+  }
+}
+
+export default postToDB;
