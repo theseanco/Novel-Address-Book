2c4e17ecf78b59746162e3820f7697e384236d73
theseanco <theseancotterill@live.com>
2020-01-30 18:41:53 +0000

    Integrated DB into Address List

---
+++ src/utilities/fetchAddresses.js
@@ -0,0 +1,16 @@
+import Axios from "axios";
+
+// Use axios to fetch data from db
+// Keeping this separate from component logic makes it more reusable and easier to test in isolation
+const fetchAddressList = async () => {
+  try {
+    // return data from axios request if we can get it
+    const { data } = await Axios.get('https://novel-address-book-backend.herokuapp.com/addresses')
+    return data;
+  } catch (error) {
+    // Catch and return error, for handling in component
+    return error;
+  }
+}
+
+export default fetchAddressList;
