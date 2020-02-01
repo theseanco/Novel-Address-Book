eb37743e932d8ae3d21b1a8f1513c8df86b04520
theseanco <theseancotterill@live.com>
2020-02-01 12:31:21 +0000

    once all fields are filled test needs to be completed

--- src/components/AddAddress/AddressForm/AddressForm.test.js
+++ src/components/AddAddress/AddressForm/AddressForm.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, getByLabelText, fireEvent, act, getByText, wait } from '@testing-library/react';
+import React, { useEffect } from 'react';
+import { render, getByLabelText, fireEvent, act, getByText, wait, getByDisplayValue } from '@testing-library/react';
 import AddressForm from './AddressForm';
 import * as addAddressToDB from '../../../utilities/addAddressToDB';
 
 jest.mock('../../../utilities/addAddressToDB');
+
+// Make a mock of the geolocation field component to test that it modifies state correctly
+jest.mock('./GeolocationField/GeolocationField', () => {
+  const ComponentToMock = ({ setLocationHook, setAddressHook, address }) => {
+    const handleSubmit = () => {
+      setLocationHook({ lat: 60, lng: 1 });
+      setAddressHook("1 London Way, London");
+    }
+    return (
+      <>
+        <input id="address" value={address} readOnly/>
+        <button onClick={handleSubmit}>Mock Autocomplete</button>
+      </>
+    )
+  };
+  return ComponentToMock;
+});
+
+// const GeolocationField = require('./ComponentToTest').default;
 
 describe('AddressForm tests', () => {
   it('Should render the correct form fields for name, notes and location', () => {
@@ -38,6 +57,38 @@ describe('AddressForm tests', () => {
         fireEvent.click(getByText(container, 'Submit'));
         await wait(() => {
           expect(getByText(container, /Please fill in/)).toBeInTheDocument();
+        })
+      });
+    })
+
+    // Here, the mocked 'button' takes the place of clicking the autoselect in lieu of a more detailed mock
+    // This is testing whether the GeolocationField component will be able to change the parent state correctly
+    it('GeolocationField comopnent changes the parent state', async () => {
+      addAddressToDB.default = jest.fn(() => []) 
+      await act(async() => {
+        const { container } = render(<AddressForm />)
+        fireEvent.click(getByText(container, 'Mock Autocomplete'));
+        await wait(() => {
+          expect(getByDisplayValue(container, /1 London Way/)).toBeInTheDocument();
+        })
+      });
+    })
+
+    it('Once all fields are filled, form can be submitted', async () => {
+      addAddressToDB.default = jest.fn(() => []) 
+      await act(async() => {
+        const { container } = render(<AddressForm />)
+        // Create fake address
+        fireEvent.click(getByText(container, 'Mock Autocomplete'));
+        // Create fake name
+        const nameInput = getByLabelText(container, /Name/);
+        fireEvent.change(nameInput, { target: { value: 'Fake Name' } } );
+        // Create fake optional notes
+        const notesInput = getByLabelText(container, /Notes/);
+        fireEvent.change(notesInput, { target: { value: 'Some notes about Sean' } } );
+        // Cick the submit button
+        await wait(() => {
+          expect(getByDisplayValue(container, /1 London Way/)).toBeInTheDocument();
         })
       });
     })
