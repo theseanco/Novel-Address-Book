4eaed42664a10686da9d340a7b835d8cfe5f0109
theseanco <theseancotterill@live.com>
2020-02-07 13:25:22 +0000

    Removed auto-created files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import Typography from 'typography';
 import moragaTheme from 'typography-theme-moraga';
 moragaTheme.baseFontSize = '15px'; // was 20px.
@@ -10,9 +9,5 @@ moragaTheme.baseFontSize = '15px'; // was 20px.
 const typography = new Typography(moragaTheme)
 typography.injectStyles();
 
+// Load the app into root div
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
