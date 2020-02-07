2d81652938b6452ff76a4f1ffd84177d91dc0188
theseanco <theseancotterill@live.com>
2020-02-07 17:28:51 +0000

    handleSelect still needs testing

--- src/components/AddressForm/GeolocationField/GeolocationField.test.js
+++ src/components/AddressForm/GeolocationField/GeolocationField.test.js
@@ -1,4 +1,4 @@
-
+// currently does not test loading state but it is done in coverage
 import GeolocationField from './GeolocationField';
 import { render, getByText, getByPlaceholderText, fireEvent, getByTestId, wait } from '@testing-library/react';
 import React from 'react';
@@ -38,9 +38,30 @@ describe('GeolocationField tests', () => {
     // re-render with the expected props now that we know we've called it correctly, this will trigger the mock function to produce fake results
     rerender(<GeolocationField setLocationHook={() => {}} setAddressHook={fakeAddressHook} address='fake'/>);
     await wait(() => {
+      // Check they have all been rendered
       expect(getByText(container, 'fake place 1')).toBeInTheDocument();
       expect(getByText(container, 'fake place 2')).toBeInTheDocument();
       expect(getByText(container, 'fake place 3')).toBeInTheDocument();
+    })
+  })
+
+  it('should be able to select and click on one of the options', async () => {
+    const fakeAddressHook = jest.fn();
+    const fakeLocationHook = jest.fn();
+    const { container, rerender } = render(<GeolocationField setLocationHook={fakeLocationHook} setAddressHook={fakeAddressHook} address=''/>)
+    // same inputs as before, producing the results
+    const input = getByTestId(container, 'addressInput');
+    fireEvent.change(input, { target: { value: 'fake' } } );
+    rerender(<GeolocationField setLocationHook={fakeLocationHook} setAddressHook={fakeAddressHook} address='fake'/>);
+    await wait(() => {
+      // Mouse into the field we want to acitvate
+      const activatableResult = getByText(container, 'fake place 3')
+      fireEvent.mouseEnter(activatableResult);
+      // Check the activatable result has the active style and others have inactive style
+      expect(activatableResult).toHaveStyle(`background-color: #41b6e6`);
+      expect(getByText(container, 'fake place 1')).toHaveStyle(`background-color: #fff`);
+      expect(getByText(container, 'fake place 2')).toHaveStyle(`background-color: #fff`);
+      // Click the activatable result to trigger the handleSelect method
     })
   })
 
